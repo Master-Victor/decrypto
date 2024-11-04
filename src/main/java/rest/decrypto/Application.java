872f20af4b41444cbac47d693fd63336b8dab4fe
victorvillafane <victor.villafane@davinci.edu.ar>
2024-11-04 11:13:40 -0300

    aws - se quito los controladores de prueba

--- src/main/java/rest/decrypto/Application.java
+++ src/main/java/rest/decrypto/Application.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import rest.decrypto.controller.PingController;
-
 
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "rest.decrypto.controller")
-@Import({ PingController.class })
 public class Application {
 
     public static void main(String[] args) {
