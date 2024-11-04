3719e2b7b35e3130a1bf0e5aa82fdb78276fd8c6
victorvillafane <victor.villafane@davinci.edu.ar>
2024-11-04 11:16:17 -0300

    url - se cambio la url para cargar el swagger en el lambda

--- src/main/java/rest/decrypto/controller/SwaggerController.java
+++ src/main/java/rest/decrypto/controller/SwaggerController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//@Controller Descomentar para desplegar en AWS
+//@Controller //Descomentar para desplegar en AWS
 /* @Profile("aws") */
 public class SwaggerController {
     @GetMapping("/swagger-ui.html")
